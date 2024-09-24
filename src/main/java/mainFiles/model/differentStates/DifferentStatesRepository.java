479984a32f14bb08949cbe3a564acde115d4a859
RuslanName <ruslangeroy@gmail.com>
2024-09-24 18:42:20 +0300

    Delete src directory

--- src/main/java/mainFiles/model/differentStates/DifferentStatesRepository.java
+++
@@ -1,8 +0,0 @@
-package mainFiles.model.differentStates;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("differentStatesRepository")
-public interface DifferentStatesRepository extends CrudRepository<DifferentStates, Long> {
-}
--- src/main/java/mainFiles/model/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package mainFiles.model.user;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("userRepository")
-public interface UserRepository extends CrudRepository<User, Long> {
-}

04c1bc5d62287427db944a82a69190f52e106942
RuslanName <ruslangeroy@gmail.com>
2024-10-13 21:00:19 +0300

    Delete src directory

--- src/main/java/mainFiles/model/registrationState/RegistrationStateRepository.java
+++
@@ -1,8 +0,0 @@
-package mainFiles.model.registrationState;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegistrationStateRepository extends CrudRepository<RegistrationState, Long> {
-}

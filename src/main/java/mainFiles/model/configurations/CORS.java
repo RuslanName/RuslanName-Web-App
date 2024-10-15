ce494b4f8fce2624e23471b81b33f286f0c188cc
RuslanName <ruslangeroy@gmail.com>
2024-10-15 23:43:56 +0300

    Create CORS.java

---
+++ src/main/java/mainFiles/model/configurations/CORS.java
@@ -0,0 +1,17 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")  // Укажите путь к вашему API
+                .allowedOrigins("https://magazin-ruslanname.amvera.io") // Разрешить только этот домен
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Укажите методы, которые разрешены
+                .allowedHeaders("*") // Разрешить все заголовки
+                .allowCredentials(true); // Разрешить отправку cookie (если нужно)
+    }
+}

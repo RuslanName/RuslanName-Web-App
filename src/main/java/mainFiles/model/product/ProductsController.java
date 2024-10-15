0ba90a4ab7e8177a676c43afad23b6cae9df57f5
RuslanName <ruslangeroy@gmail.com>
2024-10-15 23:20:09 +0300

    Update ProductsController.java

--- src/main/java/mainFiles/model/product/ProductsController.java
+++ src/main/java/mainFiles/model/product/ProductsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/products_data")
 public class ProductsController {
 
     @Autowired

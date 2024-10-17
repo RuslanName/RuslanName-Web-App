8a552e2bc6b9201f5e623f8231b13e93a6ddcd09
RuslanName <ruslangeroy@gmail.com>
2024-10-17 23:52:50 +0300

    Delete src/main/java/mainFiles directory

--- src/main/java/mainFiles/model/userProducts/UserProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package mainFiles.model.userProducts;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("userProductsRepository")
-public interface UserProductsRepository extends CrudRepository<UserProduct, Integer> {
-    UserProduct findByChatIdAndProductId(long chatId, int productId);
-}

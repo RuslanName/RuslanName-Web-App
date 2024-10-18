64cc8b65834503363339bd01c69d6e8eff111ddf
RuslanName <ruslangeroy@gmail.com>
2024-10-18 11:42:06 +0300

    Delete src directory

--- src/main/mainFiles/model/user/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package mainFiles.model.user;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("usersRepository")
-public interface UsersRepository extends CrudRepository<User, Long> {
-}
--- src/main/mainFiles/model/userCarts/UserCartsRepository.java
+++
@@ -1,9 +0,0 @@
-package mainFiles.model.userCarts;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("userProductsRepository")
-public interface UserCartsRepository extends CrudRepository<UserCart, Integer> {
-    UserCart findByChatIdAndProductId(long chatId, int productId);
-}

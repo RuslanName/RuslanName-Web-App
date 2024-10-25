94f973df0f4474f455095f0bd6d8cb75c065481a
RuslanName <ruslangeroy@gmail.com>
2024-10-26 00:58:06 +0300

    Update UserCartsRepository.java

--- src/main/java/mainFiles/model/userCarts/UserCartsRepository.java
+++ src/main/java/mainFiles/model/userCarts/UserCartsRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("userProductsRepository")
-public interface UserCartsRepository extends CrudRepository<UserCart, Integer> {
+public interface UserCartsRepository extends CrudRepository<UserCart, Long> {
     UserCart findByChatIdAndProductId(long chatId, int productId);
 }

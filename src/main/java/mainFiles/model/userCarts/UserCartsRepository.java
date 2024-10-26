f0a6908166e5461b35ddf27d105fe105da01727e
RuslanName <ruslangeroy@gmail.com>
2024-10-27 00:58:35 +0300

    Update UserCartsRepository.java

--- src/main/java/mainFiles/model/userCarts/UserCartsRepository.java
+++ src/main/java/mainFiles/model/userCarts/UserCartsRepository.java
@@ -1,3 +1,5 @@
+package mainFiles.model.userCarts;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;

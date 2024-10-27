c76d45817edc061c3e1370131db316ba95074d52
RuslanName <ruslangeroy@gmail.com>
2024-10-27 17:33:44 +0300

    Update UserCartController.java

--- src/main/java/mainFiles/model/userCarts/UserCartController.java
+++ src/main/java/mainFiles/model/userCarts/UserCartController.java
@@ -8,13 +8,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/user_carts_data")
+@CrossOrigin(origins = "https://magazin-ruslanname.amvera.io")
 public class UserCartController {
 
     @Autowired
     private UserCartService userCartService;
 
     @PostMapping("/update")
-    public String addProductToCart(@RequestParam long chatId, @RequestParam int productId, @RequestParam int quantity) {
+    public String addProductToCart(
+        @RequestParam long chatId, 
+        @RequestParam int productId, 
+        @RequestParam int quantity) {
+        
         userCartService.updateProductQuantity(chatId, productId, quantity);
         return "Product added to cart successfully!";
     }

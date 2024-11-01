cc5be70662409360ba2bbd9182c85a7a59a1a4f2
RuslanName <ruslangeroy@gmail.com>
2024-11-01 18:18:03 +0300

    Update DifferentState.java

--- src/main/java/mainFiles/model/differentState/DifferentState.java
+++ src/main/java/mainFiles/model/differentState/DifferentState.java
@@ -22,4 +22,31 @@ public class DifferentState {
     private String productName;
     private Integer productQuantity;
     private Integer productPrice;
+    private byte[] productImage;
 }
+
+// package mainFiles.model.differentState;
+
+// import lombok.Getter;
+// import lombok.Setter;
+
+// import javax.persistence.Entity;
+// import javax.persistence.Id;
+
+// @Getter
+// @Setter
+// @Entity(name = "differentStatesData")
+// public class DifferentState {
+
+//     @Id
+//     private Long chatId;
+
+//     private String action;
+
+//     private String userName;
+//     private String userPhoneNumber;
+
+//     private String productName;
+//     private Integer productQuantity;
+//     private Integer productPrice;
+// }

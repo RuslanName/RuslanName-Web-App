bae2774f94d964fdf6162778cca534fca01ed039
RuslanName <ruslangeroy@gmail.com>
2024-11-01 23:47:21 +0300

    Add files via upload

---
+++ src/main/java/mainFiles/model/differentState/DifferentState.java
@@ -0,0 +1,27 @@
+package mainFiles.model.differentState;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity(name = "differentStatesData")
+public class DifferentState {
+
+    @Id
+    private Long chatId;
+
+    private String action;
+
+    private String userName;
+    private String userData;
+
+    private Integer productId;
+    private String productName;
+    private Integer productQuantity;
+    private Integer productPrice;
+    private byte[] productImage;
+}

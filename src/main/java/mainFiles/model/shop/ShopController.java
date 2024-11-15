67600f43ab6caaa912b596c722ef35da22b033fc
RuslanName <ruslangeroy@gmail.com>
2024-11-16 01:49:58 +0300

    Update ShopController.java

--- src/main/java/mainFiles/model/shop/ShopController.java
+++ src/main/java/mainFiles/model/shop/ShopController.java
@@ -1,26 +1,16 @@
 package mainFiles.model.shop;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/shop")
 @PropertySource("application.properties")
 public class ShopController {
 
-    @Value("${shop.url}")
-    private String shopUrl;
-
-    @GetMapping("/info")
-    public Map<String, String> getShopInfo() {
-        return Map.of(
-                "message", "Добро пожаловать в магазин!",
-                "shopUrl", shopUrl
-        );
-    }
+    @Value("${webApp.url}")
+    private String webAppUrl;
 }
 

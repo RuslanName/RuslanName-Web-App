205b67c70a1b352ea712a9cc74c709a8c1e15f0f
RuslanName <ruslangeroy@gmail.com>
2024-11-16 01:18:30 +0300

    Add files via upload

---
+++ src/main/java/mainFiles/model/shop/ShopController.java
@@ -0,0 +1,14 @@
+package mainFiles.model.shop;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/shop")
+public class ShopController {
+
+    @Value("${webApp.url}")
+    private String webAppUrl;
+}
+

6d5adfab6f2abb16f46c307eda2bfbe68e46653a
RuslanName <ruslangeroy@gmail.com>
2024-11-21 22:44:29 +0300

    Delete src/main/java/mainFiles directory

--- src/main/java/mainFiles/tables/product/ProductsRepository.java
+++
@@ -1,8 +0,0 @@
-package mainFiles.tables.product;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("productsRepository")
-public interface ProductsRepository extends CrudRepository<Product, Integer> {
-}

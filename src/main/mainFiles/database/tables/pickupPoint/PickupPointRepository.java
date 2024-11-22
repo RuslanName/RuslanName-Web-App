7a72f775263af4bd40dd7a7a5258b7e4a8652f18
RuslanName <ruslangeroy@gmail.com>
2024-11-22 19:28:48 +0300

    Delete src/main/mainFiles directory

--- src/main/mainFiles/database/tables/pickupPoint/PickupPointRepository.java
+++
@@ -1,8 +0,0 @@
-package mainFiles.database.tables.pickupPoint;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("pickupPointsRepository")
-public interface PickupPointRepository extends CrudRepository<PickupPoint, Integer> {
-}

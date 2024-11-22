691981f211077560121d980790fe8c1b6299d05c
RuslanName <ruslangeroy@gmail.com>
2024-11-22 23:54:33 +0300

    Delete src/main/java/mainFiles directory

--- src/main/java/mainFiles/database/tables/userOrders/UserOrdersRepository.java
+++
@@ -1,9 +0,0 @@
-package mainFiles.database.tables.userOrders;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("userOrdersRepository")
-public interface UserOrdersRepository extends CrudRepository<UserOrder, Integer> {
-    UserOrder findByChatIdAndProductId(long chatId, int productId);
-}

4ed5cdc64fbe91abef074c44e26c4929d1fa4e52
RuslanName <ruslangeroy@gmail.com>
2024-11-22 16:58:16 +0300

    Add files via upload

---
+++ src/main/mainFiles/controllers/UserCartController.java
@@ -0,0 +1,46 @@
+package mainFiles.controllers;
+
+import mainFiles.database.tables.userCarts.UserCart;
+import mainFiles.services.WebService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user_carts_data")
+@CrossOrigin(origins = "${web.url}")
+public class UserCartController {
+
+    @Value("${web.url}")
+    private String webURL;
+
+    @Autowired
+    private WebService webService;
+
+    @PostMapping("/update")
+    public String addProductToCart(
+            @RequestParam long chatId,
+            @RequestParam int productId,
+            @RequestParam int quantity) {
+
+        webService.updateProductQuantity(chatId, productId, quantity);
+        return "Product added to cart successfully!";
+    }
+
+    @PostMapping("/update_selection")
+    public String updateProductSelection(
+            @RequestParam long chatId,
+            @RequestParam int productId,
+            @RequestParam boolean selected) {
+
+        webService.updateProductSelection(chatId, productId, selected);
+        return "Product selection updated successfully!";
+    }
+
+    @GetMapping("/load")
+    public List<UserCart> getUserCart(@RequestParam long chatId) {
+        return webService.getUserCartByChatId(chatId);
+    }
+}
---
+++ src/main/mainFiles/controllers/UsersController.java
@@ -0,0 +1,34 @@
+package mainFiles.controllers;
+
+import mainFiles.database.tables.user.User;
+import mainFiles.database.tables.user.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class UsersController {
+    @Value("${web.url}")
+    private String webURL;
+
+    @Autowired
+    private UsersRepository userRepository;
+
+    @GetMapping("/check_user")
+    @CrossOrigin(origins = "${web.url}")
+    public Map<String, Boolean> checkUserRegistration(@RequestParam String chatId) {
+        Optional<User> user = userRepository.findById(Long.valueOf(chatId));
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("isRegistered", user.isPresent());
+
+        return response;
+    }
+}
+
